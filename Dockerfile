d2412784cc8b038b1bf721b335653cdeda382715
kotahorii <khorii.micron@gmail.com>
2022-03-11 19:29:05 +0900

    [update] main.go

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:1.17.8-buster
 
 RUN apt-get update && \
     apt-get install git && \
-    apt install sudo \
+    apt install sudo && \
     apt-get install lsof
 
 WORKDIR /go/src/app
--- app/main.go
+++ app/main.go
@@ -1,56 +1,11 @@
 package main
 
 import (
+	"docker_sample/controller"
 	"docker_sample/database"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
-
-type User struct {
-	Id    int    `json:"id" param:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-func getUsers(c echo.Context) error {
-	users := []User{}
-	database.DB.Find(&users)
-	return c.JSON(http.StatusOK, users)
-}
-
-func getUser(c echo.Context) error {
-	user := User{}
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	database.DB.Take(&user)
-	return c.JSON(http.StatusOK, user)
-}
-
-func createUser(c echo.Context) error {
-	user := User{}
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	database.DB.Create(&user)
-	return c.JSON(http.StatusCreated, user)
-}
-
-func updateUser(c echo.Context) error {
-	user := User{}
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	database.DB.Save(&user)
-	return c.JSON(http.StatusOK, user)
-}
-
-func deleteUser(c echo.Context) error {
-	id := c.Param("id")
-	database.DB.Delete(&User{}, id)
-	return c.NoContent(http.StatusNoContent)
-}
 
 func main() {
 	e := echo.New()
@@ -58,11 +13,11 @@ func main() {
 	sqlDB, _ := database.DB.DB()
 	defer sqlDB.Close()
 
-	e.GET("/users", getUsers)
-	e.GET("/users/:id", getUser)
-	e.POST("/users", createUser)
-	e.PUT("/users/:id", updateUser)
-	e.DELETE("/users/:id", deleteUser)
+	e.GET("/users", controller.GetUsers)
+	e.GET("/users/:id", controller.GetUser)
+	e.POST("/users", controller.CreateUser)
+	e.PUT("/users/:id", controller.UpdateUser)
+	e.DELETE("/users/:id", controller.DeleteUser)
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
