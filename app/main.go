fa6de1f0047f07913ddc1b8a80b5f3ddc6662280
kotahorii <khorii.micron@gmail.com>
2022-03-11 13:41:44 +0900

    [update] database connection successfully

--- app/main.go
+++ app/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"docker_sample/database"
+	"fmt"
+)
+
+type User struct {
+	Id    int    `json:"id" param:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
-	fmt.Println("Hello golang from docker!")
+	db := database.Connect()
+	defer db.Close()
+
+	err := db.Ping()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	} else {
+		fmt.Println("データベース接続成功")
+	}
 }
